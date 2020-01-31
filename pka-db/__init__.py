73edc08c19a708b3bac06965457ec3ce2de9bb98
Max Cohn <maxc4321@gmail.com>
2020-01-30 22:45:38 -0500

    Created guest page and db access functions

--- pka-db/__init__.py
+++ pka-db/__init__.py
@@ -2,11 +2,90 @@ from flask import Flask, render_template, request, g
 from . import db
 import sqlite3
 import os
+import datetime
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATABASE_PATH = os.path.join(BASE_DIR, "main.db")
 #DATABASE_PATH = 'main.db'
+
+
+@app.route('/')
+def home():
+    return render_template('base.html')
+
+@app.route('/pka/<episode>', methods=['GET'])
+def get_pka_epsiode(episode):
+    '''Page consisting of list of guests present on the episode and a list of
+    events from that episode.
+
+    :param episode: Episode number
+    '''
+    cur = get_db().cursor()
+
+    guest_list = db.all_episode_guests(cur, 'pka', episode)
+
+    guest_list = [{'id': x[0], 'name': x[1]} for x in guest_list]
+  
+    
+    
+    cur.close()
+
+    return render_template(
+        'episode.html',
+        show_name='PKA',
+        episode=episode,
+        guest_list=guest_list
+    )
+
+
+@app.route('/guest/id/<guest_id>', methods=['GET'])
+def get_guest(guest_id: int):
+    cur = get_db().cursor()
+    
+    # get list of dicts of episode appearances
+    tmp_appearances = db.all_guest_appearances_by_id(cur, guest_id)
+    appearance_list = [{'show_name': 'PKA' if x[0] == 1 else 'PKN','episode': x[1]} for x in tmp_appearances]
+    
+    # get total runtime and convert to a nicer format
+    total_runtime = db.total_guest_runtime(cur, guest_id)
+
+    seconds = total_runtime % 60
+    total_runtime = total_runtime // 60
+    minutes = total_runtime % 60
+    hours = total_runtime // 60
+
+    runtime_str = f'{hours} hours, {minutes} minutes, and {seconds} seconds'
+
+    # get the guest's name    
+    guest_name = db.guest_name_by_id(cur, guest_id)
+
+    cur.close()
+
+    return render_template(
+        'guest.html', 
+        appearance_list=appearance_list, 
+        runtime=runtime_str,
+        guest_name=guest_name    
+    )
+
+
+
+"""
+#TODO change this to a search
+@app.route('/guest/name/<guest_name>')
+def get_guest_by_name(guest_name):
+    cur = get_db().cursor()
+    guest_list = db.all_guest_appearances_by_name(cur, guest_name)
+    cur.close()
+    return '\n'.join(map(lambda x: f'<li>{x[1]}</li>', guest_list))
+"""
+
+
+
+
+
+
 
 # get database from context
 def get_db():
@@ -21,41 +100,6 @@ def close_connection(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
-@app.route('/')
-def home():
-    return render_template('base.html')
-
-@app.route('/pka/<episode>')
-def get_pka_epsiode(episode):
-    '''Page consisting of list of guests present on the episode and a list of
-    events from that episode.
-
-    :param episode: Episode number
-    '''
-    cur = get_db()
-    guest_list = db.all_episode_guests(cur, 'pka', episode)
-    cur.close()
-
-    return '\n'.join(map(lambda x: f'<li>{x[1]}</li>', guest_list))
-
-
-@app.route('/guests/id/<guest_id>')
-def guest(guest_id: int):
-
-    cur = get_db().cursur()
-    
-    #TODO finish this page
-    render_template('guests.html')
-
-#TODO change this to a search
-@app.route('/guest/name/<guest_name>')
-def get_guest_by_name(guest_name):
-    cur = get_db().cursor()
-    guest_list = db.all_guest_appearances_by_name(cur, guest_name)
-    cur.close()
-    return '\n'.join(map(lambda x: f'<li>{x[1]}</li>', guest_list))
-
 
 if __name__ == '__main__':
     app.run()
--- pka-db/db.py
+++ pka-db/db.py
@@ -20,10 +20,18 @@ def all_episode_events(cur: sqlite3.Cursor, show: str, ep_num: int) -> List[Tupl
 
     return cur.execute('''
     select timestamp, description from events
-    where show = ? and episode = ?;
+    where show = ? and episode = ?
     ''', (show_id, ep_num)).fetchall()
 
 def all_episode_guests(cur: sqlite3.Cursor, show: str, ep_num: int) -> List[Tuple[int, str]]:
+    '''Gets all guests that were on a given episode
+
+    :param cur: DB cursor
+
+    :param show: Name of the show ('pka' or 'pkn')
+
+    :param ep_num: Episode number
+    '''
     show = show.lower()
     show_id = 0
     if show == 'pka':
@@ -39,21 +47,70 @@ def all_episode_guests(cur: sqlite3.Cursor, show: str, ep_num: int) -> List[Tupl
     where guest_id in (
         select guest_id from appearances
         where appearances.show = ? and appearances.episode = ?
-    );
+    )
     ''', (show_id, ep_num)).fetchall()
 
 
-#TODO use cur
-def all_guest_appearances_by_id(guest_id: int) -> List[Tuple[int, int]]:
-    conn = sqlite3.connect('main.db')
+def all_guest_appearances_by_id(cur: sqlite3.Cursor, guest_id: int) -> List[Tuple[int, int]]:
+    '''Finds all apperances of a given guest id
 
-    return conn.execute('''select show, episode from appearances
+    :param cur: DB cursor
+
+    :param guest_id: Guest id in the DB
+    '''
+    return cur.execute('''
+    select show, episode from appearances
     where appearances.guest_id in (
 	    select guest_id from guests
 	    where guests.guest_id = ?
-    )''', (guest_id,))
+    )
+    order by episode desc
+    ''', (guest_id,))
+
+def guest_name_by_id(cur: sqlite3.Cursor, guest_id: int) -> str:
+
+    return cur.execute('''
+    select name from guests
+    where guest_id = ?
+    ''', (guest_id,)).fetchone()[0]
+
+def total_guest_runtime(cur: sqlite3.Cursor, guest_id: int) -> int:
+
+    pka_runtime = cur.execute('''
+    select sum(runtime) from episodes
+    where episode in (
+        select episode from appearances
+        where guest_id = ?
+    )
+    and show = 1
+    ''', (guest_id,)).fetchone()[0]
+
+    print(f'pka runtime: {pka_runtime}', flush=True)
+
+    pkn_runtime = cur.execute('''
+    select sum(runtime) from episodes
+    where episode in (
+        select episode from appearances
+        where guest_id = ?
+    )
+    and show = 2
+    ''', (guest_id,)).fetchone()[0]
+
+    if pka_runtime is None:
+        pka_runtime = 0
+
+    if pkn_runtime is None:
+        pkn_runtime = 0
 
 
+
+    print(f'pkn runtime: {pkn_runtime}', flush=True)
+
+    return pka_runtime + pkn_runtime
+
+
+
+"""
 #TODO change this toa  search
 def all_guest_appearances_by_name(cur: sqlite3.Cursor, guest_name: str) -> List[Tuple[int, int]]:
     '''Get each apperance (show_id, episode_num) of all guests that match `guest_name`
@@ -64,12 +121,13 @@ def all_guest_appearances_by_name(cur: sqlite3.Cursor, guest_name: str) -> List[
     '''
     guest_name = f'%{guest_name}%'
 
-    return cur.execute('''select show, episode from appearances
+    return cur.execute('''
+    select show, episode from appearances
     where appearances.guest_id in (
 	    select guest_id from guests
 	    where guests.name like ?
     )
     order by episode asc''', (guest_name,)).fetchall()
-
+"""
 #print(2 + 2)
 #print(all_guest_appearances_by_name(sqlite3.connect('main.db').cursor(), 'Awz'))
